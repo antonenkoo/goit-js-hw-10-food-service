765220852c50f33ad774a72756bffa6d7f49f4ae
Mark Antonenko <2002mark.a@gmail.com>
2022-05-04 08:58:12 +0300

    hw10

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
 import './sass/main.scss';
+import menu from './json/menu.json';
+import cardTpl from './templates/card.hbs';
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+themeEdit();
+
+
+document.getElementById('theme-switch-toggle').onclick = function () {
+  let input = document.getElementById('theme-switch-toggle');
+  if (input.checked) {
+    document.body.classList.replace(Theme.LIGHT, Theme.DARK);
+    localStorage.setItem('theme', JSON.stringify('dark'));
+  } else {
+    document.body.classList.replace(Theme.DARK, Theme.LIGHT);
+    localStorage.setItem('theme', JSON.stringify('light'));
+  }
+};
+
+function themeEdit() {
+  const theme = localStorage.getItem('theme');
+  const currentTheme = JSON.parse(theme);
+
+  if (currentTheme === 'dark') {
+    document.body.classList.replace(Theme.LIGHT, Theme.DARK);
+    localStorage.setItem('theme', JSON.stringify('dark'));
+    document.getElementById('theme-switch-toggle').checked = true;
+  } else {
+    document.body.classList.replace(Theme.DARK, Theme.LIGHT);
+    localStorage.setItem('theme', JSON.stringify('light'));
+    document.getElementById('theme-switch-toggle').checked = false;
+  }
+}
+
+let jsMenu = document.querySelector('.js-menu');
+jsMenu.insertAdjacentHTML('beforeend', cardTpl(menu));
